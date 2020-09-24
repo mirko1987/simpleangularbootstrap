4aba7167e732023f66f233446fdbb5cb87b159ba
mirko uriel <mirkouriel@MBP-di-mirko.home>
2020-09-24 23:23:56 +0200

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'bootstrap0'`, () => {
+  it(`should have as title 'bootstrap'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('bootstrap0');
+    expect(app.title).toEqual('bootstrap');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('bootstrap0 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('bootstrap app is running!');
   });
 });
